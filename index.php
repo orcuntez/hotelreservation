eebc414f4e6bacfe687ae682db9a26378a9364ba
orcun <orcun@10.42.0.112>
2016-11-16 12:34:52 +0200

    css found for shaped

---
+++ index.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html >
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>HotelRezervationSystem</title>
+
+  
+  
+  <link href="font-awesome.min.css" rel="stylesheet">
+  <link href="bootstrap.min.css" rel="stylesheet">
+  <link href="bootstrap-datetimepicker.min.css" rel="stylesheet">  
+  <link href="flexslider.css" rel="stylesheet">
+  <link href="templatemo-style.css" rel="stylesheet">
+
+  
+
+  </head>
+  <body class="tm-gray-bg">
+  	<!-- Header -->
+  	<div class="tm-header">
+  		<div class="container">
+  			<div class="row">
+  				<div class="col-lg-6 col-md-4 col-sm-3 tm-site-name-container">
+  					<a href="#" class="tm-site-name">Hotel Rezervation System</a>	
+  				</div>
+	  			<div class="col-lg-6 col-md-8 col-sm-9">
+	  				<div class="mobile-menu-icon">
+		              <i class="fa fa-bars"></i>
+		            </div>
+	  				<nav class="tm-nav">
+						<ul>
+							<li><a href="index.php" class="active">Home</a></li>
+							<li><a href="about.html">About</a></li>
+						
+                                                        <li><a href="register.php">Register</a></li>
+                                                        <li><a href="login.php">Login</a></li>
+						</ul>
+					</nav>		
+	  			</div>				
+  			</div>
+  		</div>	  	
+  	</div>
+	
+	
+	
+		
+			    <div class="tm-banner-inner">
+					<h1 class="tm-banner-title">Find <span class="tm-yellow-text">The Best</span> Hotel</h1>
+					
+					
+				</div>
+				<img src="banner-1.jpg" alt="Image" />	
+		   
+
+
+	<!-- gray bg -->	
+	<section class="container tm-home-section-1" id="more">
+		<div class="row">
+			<div class="col-lg-4 col-md-4 col-sm-6">
+				<!-- Nav tabs -->
+				<div class="tm-home-box-1">
+					<ul class="nav nav-tabs tm-white-bg" role="tablist" id="hotelCarTabs">
+					    <li role="presentation" class="active">
+					    	<a href="#hotel" aria-controls="hotel" role="tab" data-toggle="tab">Search Hotel</a>
+					    </li>
+					    
+					</ul>
+
+					<!-- Tab panes -->
+					<div class="tab-content">
+					    <div role="tabpanel" class="tab-pane fade in active tm-white-bg" id="hotel">
+					    	<div class="tm-search-box effect2">
+								<form action="#" method="post" class="hotel-search-form">
+									<div class="tm-form-inner">
+										<div class="form-group">
+							            	 
+                                                                             <input type='text' class="form-control" placeholder="-- Select Hotel --" />
+							            	 	
+										
+							          	</div>
+							          	<div class="form-group">
+							                <div class='input-group date' id='datetimepicker1'>
+							                    <input type='text' class="form-control" placeholder="Check-in Date" />
+							                    <span class="input-group-addon">
+							                        <span class="fa fa-calendar"></span>
+							                    </span>
+							                </div>
+							            </div>
+							          	<div class="form-group">
+							                <div class='input-group date' id='datetimepicker2'>
+							                    <input type='text' class="form-control" placeholder="Check-out Date" />
+							                    <span class="input-group-addon">
+							                        <span class="fa fa-calendar"></span>
+							                    </span>
+							                </div>
+							            </div>
+							            <div class="form-group margin-bottom-0">
+							                <select class="form-control">
+							            	 	<option value="">-- Guests -- </option>
+							            	 	<option value="1">1</option>
+												<option value="2">2</option>
+												<option value="3">3</option>
+												<option value="4">4</option>
+												<option value="5p">5+</option>
+											</select> 
+							            </div>
+									</div>							
+						            <div class="form-group tm-yellow-gradient-bg text-center">
+						            	<button type="submit" name="submit" class="tm-yellow-btn">Check Now</button>
+						            </div>  
+								</form>
+							</div>
+					    </div>
+		</div>
+	
+		
+	</section>
+	<footer class="tm-black-bg">
+		<div class="container">
+			<div class="row">
+				<p class="tm-copyright-text">Copyright &copy;  Hotel Rezarvation System </p>
+			</div>
+		</div>		
+	
+ </body>
+ </html>
